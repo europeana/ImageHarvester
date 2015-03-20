5350a858def2b75536400b44fa3de8571531863a
Paul Sabou <paul.sabou@gmail.com>
2015-03-20 12:01:39 +0100

    added extra server in the deployment script

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,4 @@ scp ./harvester.jar norbert@europeana0.busymachines.com:/home/norbert/harvester/
 scp ./harvester.jar norbert@europeana5.busymachines.com:/home/norbert/harvester/harvester.jar
 scp ./harvester.jar norbert@europeana6.busymachines.com:/home/norbert/harvester/harvester.jar
 scp ./harvester.jar norbert@europeana7.busymachines.com:/home/norbert/harvester/harvester.jar
+scp ./harvester.jar norbert@europeana8.busymachines.com:/home/norbert/harvester/harvester.jar
