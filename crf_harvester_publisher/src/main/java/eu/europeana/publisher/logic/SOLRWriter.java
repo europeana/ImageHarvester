58ec16425cb6f253dd6d4ba44c7181aaaa583f3d
Paul Sabou <paul.sabou@gmail.com>
2015-03-23 19:13:42 +0100

    added exception logging for failed solr writter

--- crf_harvester_publisher/src/main/java/eu/europeana/publisher/logic/SOLRWriter.java
+++ crf_harvester_publisher/src/main/java/eu/europeana/publisher/logic/SOLRWriter.java
@@ -9,7 +9,6 @@ import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.BinaryRequestWriter;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.client.solrj.util.ClientUtils;
 import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.params.CommonParams;
@@ -65,56 +64,60 @@ public class SOLRWriter {
             update.addField("facet_tags", singletonMap("set", CRFSolrDocument.getFacetTags()));
 
             try {
-             server.add(update);
+                server.add(update);
             } catch (Exception e) {
-                LOG.error("Solr exception when adding document",e);
-                LOG.error("Solr document that caused exception"+update);
+                LOG.error("Solr exception when adding document", e);
+                LOG.error("Solr document that caused exception" + update);
                 throw e;
             }
         }
 
-        LOG.info("SOLR: committing " + newDocs.size()+" documents");
+        LOG.info("SOLR: committing " + newDocs.size() + " documents");
         server.commit();
     }
 
     /**
      * Checks existence of documents in the SOLR index.
+     *
      * @param documentIds the document id's that need to be checked
      * @return the map that indicates for each document id (map key) whether exists : true or false
      * @throws SolrServerException
      */
-    public Map<String,Boolean> documentExists(final List<String> documentIds) throws SolrServerException {
-
+    public Map<String, Boolean> documentExists(final List<String> documentIds) throws SolrServerException {
 
         // Initialise the result : no documents exist
-        final Map<String,Boolean> result = new HashMap<String,Boolean>();
+        final Map<String, Boolean> result = new HashMap<String, Boolean>();
         if (documentIds.isEmpty()) {
             return result;
         }
 
-        for (final String id : documentIds) result.put(id,false);
+        for (final String id : documentIds) result.put(id, false);
 
         // As the SOLR query has limitations it cannot handle queries that are too large => we need to break them in parts
-        for (int documentIdsStartChunkIndex = 0; documentIdsStartChunkIndex<= documentIds.size(); documentIdsStartChunkIndex+= MAX_NUMBER_OF_IDS_IN_SOLR_QUERY) {
-            final int endOfArray = (documentIdsStartChunkIndex+ MAX_NUMBER_OF_IDS_IN_SOLR_QUERY >=documentIds.size())?documentIds.size():documentIdsStartChunkIndex+ MAX_NUMBER_OF_IDS_IN_SOLR_QUERY;
-            List<String> documentIdsToQuery = documentIds.subList(documentIdsStartChunkIndex,endOfArray);
+        for (int documentIdsStartChunkIndex = 0; documentIdsStartChunkIndex <= documentIds.size(); documentIdsStartChunkIndex += MAX_NUMBER_OF_IDS_IN_SOLR_QUERY) {
+            final int endOfArray = (documentIdsStartChunkIndex + MAX_NUMBER_OF_IDS_IN_SOLR_QUERY >= documentIds.size()) ? documentIds.size() : documentIdsStartChunkIndex + MAX_NUMBER_OF_IDS_IN_SOLR_QUERY;
+            List<String> documentIdsToQuery = documentIds.subList(documentIdsStartChunkIndex, endOfArray);
 
             // Do the SOLR query
-            final SolrQuery query=new SolrQuery();
-            final String queryString = "("+StringUtils.join(documentIdsToQuery," OR ").replace("/","\\/")+")";
-            query.set(CommonParams.Q,"*:*");
-            query.set(CommonParams.ROWS, MAX_NUMBER_OF_IDS_IN_SOLR_QUERY +1);
-            query.set(CommonParams.FQ,"europeana_id:" + queryString);
-            query.set(CommonParams.FL,"europeana_id");
+            final SolrQuery query = new SolrQuery();
+            final String queryString = "(" + StringUtils.join(documentIdsToQuery, " OR ").replace("/", "\\/") + ")";
+            query.set(CommonParams.Q, "*:*");
+            query.set(CommonParams.ROWS, MAX_NUMBER_OF_IDS_IN_SOLR_QUERY + 1);
+            query.set(CommonParams.FQ, "europeana_id:" + queryString);
+            query.set(CommonParams.FL, "europeana_id");
 
-            final QueryResponse response = server.query(query);
-            // Mark in the result the documents id's that have been found
-            if (response != null) {
-                final SolrDocumentList solrResults = response.getResults();
-                for (int resultEntryIndex = 0; resultEntryIndex < solrResults.size(); ++resultEntryIndex)
-                    result.put(solrResults.get(resultEntryIndex).getFieldValue("europeana_id").toString(), true);
+            try {
+                final QueryResponse response = server.query(query);
+                // Mark in the result the documents id's that have been found
+                if (response != null) {
+                    final SolrDocumentList solrResults = response.getResults();
+                    for (int resultEntryIndex = 0; resultEntryIndex < solrResults.size(); ++resultEntryIndex)
+                        result.put(solrResults.get(resultEntryIndex).getFieldValue("europeana_id").toString(), true);
+                }
+            } catch (Exception e) {
+                LOG.error("SOLR query failed when executing query " + queryString);
+                throw e;
             }
-
         }
 
 
