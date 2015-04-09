c7261f3ee3956649a929d4598920fd9c470141bf
SAlexandru <salexandru.alex@gmail.com>
2015-04-09 10:30:40 +0300

    deployment script

--- deployPublisher.sh
+++ deployPublisher.sh
@@ -6,4 +6,4 @@ mvn -Dmaven.test.skip=true -U clean install package
 cd ../crf_harvester_publisher/
 mvn -Dmaven.test.skip=true -U clean install package
 
-scp ./target/crf_harvester_publisher-0.1-SNAPSHOT-allinone.jar norbert@europeana8.busymachines.com:/home/norbert/publisher/publisher.jar
+scp ./target/crf_harvester_publisher-0.1-SNAPSHOT-allinone.jar norbert@europeana1.busymachines.com:/home/norbert/publisher/publisher.jar
