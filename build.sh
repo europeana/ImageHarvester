842115b3d94d763fef48bebee8622fdeb9b19b08
SAlexandru <salexandru.alex@gmail.com>
2015-05-06 15:45:10 +0300

    added local library dependency for harvester-server and modified the build script

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+./installMediaCheckerLibrary.sh
+
 mvn -Dmaven.test.skip=true clean install package -DskipTests
 cd ./harvester-persistence/
 mvn -Dmaven.test.skip=true clean install package -DskipTests
---
+++ installMediaCheckerLibrary.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+mvn install:install-file -Dfile=/home/salexandru/Desktop/Projects/europeana/ImageHarvester/harvester-server/libDependencies/MediaChecker-1.0-SNAPSHOT.jar -DpomFile=/home/salexandru/Desktop/Projects/europeana/ImageHarvester/harvester-server/libDependencies/pom.xml
