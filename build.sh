ae3bbb0d9920f22f03c52794a78ea07f86ab6efe
dantodor <dantodor@gmail.com>
2015-05-29 12:06:26 +0300

    Added JobCreator to build tasks

--- build.sh
+++ build.sh
@@ -5,8 +5,11 @@ cd ./harvester-persistence/
 mvn -Dmaven.test.skip=true clean install package -DskipTests
 cd ../media-storage-client/
 mvn -Dmaven.test.skip=true clean install package -DskipTests
+cd ../harvester-job-creator/
+mvn -Dmaven.test.skip=true clean install package -DskipTests
 cd ../harvester-server/
 mvn -Dmaven.test.skip=true clean install package -DskipTests
+
 cd ..
 
 mv ./harvester-server/target/harvester-server-0.1-SNAPSHOT-allinone.jar ./harvester.jar
