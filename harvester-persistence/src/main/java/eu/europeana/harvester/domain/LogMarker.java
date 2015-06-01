f90b83688a0f00572da9a6f449b4142f19378e40
Paul Sabou <paul.sabou@gmail.com>
2015-06-01 23:43:25 +0200

    added log markers

---
+++ harvester-persistence/src/main/java/eu/europeana/harvester/domain/LogMarker.java
@@ -0,0 +1,14 @@
+package eu.europeana.harvester.domain;
+
+public class LogMarker {
+
+    public static final String EUROPEANA_PROVIDER_ID = "europeana_provider_id";
+    public static final String EUROPEANA_COLLECTION_ID = "europeana_collection_id";
+    public static final String EUROPEANA_RECORD_ID = "europeana_record_id";
+    public static final String EUROPEANA_PROCESSING_JOB_ID = "europeana_processing_job_id";
+    public static final String EUROPEANA_PROCESSING_JOB_EXECUTION_ID = "europeana_processing_job_execution_id";
+    public static final String EUROPEANA_SOURCE_DOCUMENT_REFERENCE_ID = "europeana_source_document_reference_id";
+    public static final String EUROPEANA_URL = "europeana_url";
+    public static final String EUROPEANA_PROCESSING_JOB_TASK_TYPE = "europeana_processing_job_task_type";
+
+}
