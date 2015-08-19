d1b9a24eea1adfd41f6292b7aed271e57d28c1fe
SAlexandru <salexandru.alex@gmail.com>
2015-08-19 12:32:45 +0300

    use local repo for build

--- buildAllOnMaster.sh
+++ buildAllOnMaster.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-mvn -Dmaven.test.skip=true clean install package -DskipTests
+mvn -Dmaven.test.skip=true -Dmaven.repo.local=$1 clean install package -DskipTests
 cd ./harvester-persistence/
-mvn -Dmaven.test.skip=true clean install package -DskipTests                     
+mvn -Dmaven.test.skip=true -Dmaven.repo.local=$1 clean install package -DskipTests                     
 cd ../crf-fake-tags
-mvn -Dmaven.test.skip=true clean install package -DskipTests
+mvn -Dmaven.test.skip=true -Dmaven.repo.local=$1 clean install package -DskipTests
 cd ../media-storage-client/
-mvn -Dmaven.test.skip=true clean install package -DskipTests
+mvn -Dmaven.test.skip=true -Dmaven.repo.local=$1 clean install package -DskipTests
 cd ../harvester-job-creator/
-mvn -Dmaven.test.skip=true clean install package -DskipTests
+mvn -Dmaven.test.skip=true -Dmaven.repo.local=$1 clean install package -DskipTests
 cd ../harvester-server/
-mvn -Dmaven.test.skip=true clean install package -DskipTests
+mvn -Dmaven.test.skip=true -Dmaven.repo.local=$1 clean install package -DskipTests
 cd ../crf-migration/
-mvn -Dmaven.test.skip=true clean install package -DskipTests
+mvn -Dmaven.test.skip=true -Dmaven.repo.local=$1 clean install package -DskipTests
 cd ../crf_harvester_publisher/
-mvn -Dmaven.test.skip=true clean install package -DskipTests
+mvn -Dmaven.test.skip=true -Dmaven.repo.local=$1 clean install package -DskipTests
 cd ../harvester-client/
-mvn -Dmaven.test.skip=true clean install package -DskipTests
+mvn -Dmaven.test.skip=true -Dmaven.repo.local=$1 clean install package -DskipTests
 
 cd ..
 
