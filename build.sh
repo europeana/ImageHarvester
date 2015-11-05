c287e824bbb21c68f27e110a120459f9f33fbb00
Paul Sabou <paul.sabou@gmail.com>
2015-11-05 13:04:41 +0100

    added 3 extra builds

--- build.sh
+++ build.sh
@@ -13,6 +13,12 @@ cd ../harvester-client/
 mvn -Dmaven.test.skip=true clean install package -DskipTests
 cd ../harvester-server/
 mvn -Dmaven.test.skip=true clean install package -DskipTests
+cd ../crf_harvester_publisher/
+mvn -Dmaven.test.skip=true clean install package -DskipTests
+cd ../crf-migration
+mvn -Dmaven.test.skip=true clean install package -DskipTests
+cd ../uim-tester
+mvn -Dmaven.test.skip=true clean install package -DskipTests
 
 cd ..
 
