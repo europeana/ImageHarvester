25ee7d4965943d53b59a1785aaa4767309745a6b
Luthien-in-edhil <maike.dulk@europeana.eu>
2018-02-01 13:27:05 +0100

    added bluemix option in Slave.java

--- media-storage-client/src/main/java/eu/europeana/harvester/db/s3/BluemixConfiguration.java
+++ media-storage-client/src/main/java/eu/europeana/harvester/db/s3/BluemixConfiguration.java
@@ -3,11 +3,10 @@ package eu.europeana.harvester.db.s3;
 import com.typesafe.config.Config;
 
 /**
- * Created by ymamakis on 12/9/16.
+ * Created by luthien on 31/1/18.
  */
 public class BluemixConfiguration {
 
-    private String region;
     private String bucket;
     private String clientKey;
     private String secretKey;
@@ -18,8 +17,6 @@ public class BluemixConfiguration {
         clientKey = config.getString("client_key");
         if (!config.hasPath("secret_key")) throw new IllegalArgumentException("The configuration is missing the secret_key property");
         secretKey = config.getString("secret_key");
-        if (!config.hasPath("region")) throw new IllegalArgumentException("The configuration is missing the region property");
-        region = config.getString("region");
         if (!config.hasPath("bucket")) throw new IllegalArgumentException("The configuration is missing the bucket property");
         bucket = config.getString("bucket");
         if (!config.hasPath("endpoint")) throw new IllegalArgumentException("The configuration is missing the endpoint property");
@@ -27,9 +24,6 @@ public class BluemixConfiguration {
     }
     public static BluemixConfiguration valueOf(final Config config){
         return  new BluemixConfiguration(config);
-    }
-    public String getRegion(){
-        return region;
     }
 
     public String getBucket() {
